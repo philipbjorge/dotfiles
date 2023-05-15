bb2d45ab0c8b9180944024ab63bb76f420256dbc
Philip Bjorge <github@philipbjorge.com>
2023-05-15 10:06:54 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,15 +4,10 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin main;
 
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-}
-
-doIt;
-unset doIt;
+rsync --exclude ".git/" \
+	--exclude ".DS_Store" \
+	--exclude ".osx" \
+	--exclude "bootstrap.sh" \
+	--exclude "README.md" \
+	--exclude "LICENSE-MIT.txt" \
+	-avh --no-perms . ~;
